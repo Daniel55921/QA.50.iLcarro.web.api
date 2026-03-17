18cdc5c041908f8cdc3cbcbbed64d5c00d2ceb08
Daniil <manevich95@gmail.com>
2026-03-17 21:00:46 +0200

    Adding Logs & search(town/date)

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import utils.PropertiesReader;
+
+import java.time.LocalDate;
+
 import static utils.PropertiesReader.*;
 
 public class HomePage extends BasePage {
@@ -20,6 +23,12 @@ public class HomePage extends BasePage {
     WebElement btnLogin;
     @FindBy(xpath = "//a[text() ='Sign up']")
     WebElement btnSignUp;
+    @FindBy(id = "city")
+    WebElement inputCity;
+    @FindBy(id = "dates")
+    WebElement inputDates;
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement btnYalla;
 
     public void clickBtnLogin(){
         btnLogin.click();
@@ -27,5 +36,19 @@ public class HomePage extends BasePage {
 
     public void clickBtnSignUp(){
         btnSignUp.click();
+    }
+
+    public void typeSearchForm(String city,
+                               LocalDate startDate, LocalDate endDate) {
+        inputCity.sendKeys(city);
+        System.out.println(startDate.toString());
+        String dates = startDate.getMonthValue()+"/"
+                + startDate.getDayOfMonth()+"/"
+                + startDate.getYear()+" - "
+                + endDate.getMonthValue()+"/"
+                + endDate.getDayOfMonth()+"/"
+                + endDate.getYear();
+        inputDates.sendKeys(dates);
+        btnYalla.click();
     }
 }
